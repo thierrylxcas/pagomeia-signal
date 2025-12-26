a404e821e4f8d7f81fca2116f2c527430277d62a
thierrylxcas <160182106+thierrylxcas@users.noreply.github.com>
2025-12-25 22:35:05 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,45 +1,13 @@
-const SUPABASE_URL = "https://yxeowgpwumbirppmssmd.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_JYqJrlOtlH12J1xhgAaObw_MU4R8P0-";
-
-const supabase = supabaseJs.createClient(
-  SUPABASE_URL,
-  SUPABASE_ANON_KEY
-);
-
-const chat = document.getElementById("chat");
-const usernameInput = document.getElementById("username");
-const msgInput = document.getElementById("msg");
-
-let username = "";
+alert("JS carregou com sucesso!");
 
 function join(){
-  username = usernameInput.value.trim();
-  if(!username) return alert("Digite um nome");
-  listenChat();
-  alert("Entrou no chat");
+  alert("Botão ENTRAR funcionando");
 }
 
 function sendMsg(){
-  const text = msgInput.value.trim();
-  if(!text) return;
-
-  supabase.from("messages").insert({
-    username,
-    text
-  });
-
-  msgInput.value = "";
+  alert("Botão ENVIAR funcionando");
 }
 
-function listenChat(){
-  supabase.channel("chat")
-    .on(
-      "postgres_changes",
-      { event:"INSERT", schema:"public", table:"messages" },
-      payload => {
-        const m = payload.new;
-        chat.innerHTML += `<div><b>${m.username}:</b> ${m.text}</div>`;
-      }
-    )
-    .subscribe();
+function startShare(){
+  alert("Botão COMPARTILHAR funcionando");
 }
